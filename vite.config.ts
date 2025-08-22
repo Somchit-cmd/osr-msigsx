da5b8745a232ed59327dede9f0dd4570eb96f93e
Somchit-cmd <somchitktv24@gmail.com>
2025-08-22 11:38:29 +0700

    try

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: "/osr-msigsx",
+  base: "/osr-msigsx/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
